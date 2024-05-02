5aa42c517651d8d4b41b9d2e35db9ca49276b925
Divsha14 <146617489+Divsha14@users.noreply.github.com>
2024-05-02 16:28:32 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1445
+let accountEmail = "divya@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+//accountId = 2; Assignment to constant variable. -> not allowed
+console.log("accountId") 
+
+accountEmail = "sharma@gmail.com"
+accountPassword = "6789"
+accountCity = "Delhi"
+
+//var is not used because of its scope
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict" //treat all js code as new version
+//alert(3+3) using node not browser 
+
+
+/* 
+number = 1
+null 
+bigint -> used where big numbers are used
+boolean -> t/f
+string = ""
+symbol -> unique
+undefined
+*/
+
+// object -> non-primitive
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
