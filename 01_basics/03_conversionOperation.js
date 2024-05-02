962cfd7f778acf34969144dbeaf3bce1790df51f
Divsha14 <146617489+Divsha14@users.noreply.github.com>
2024-05-02 17:15:46 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+ let score = "33abcd"
+ let age = null
+ let score2 = undefined
+
+
+ console.log(typeof score)
+ console.log(typeof (score))
+ let valueInNumber = Number(score);
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+console.log(typeof age)
+let valueInNum = Number(age);
+console.log(typeof valueInNum)
+console.log(valueInNum)
+
+console.log(typeof score2)
+let valueInNumb = Number(score2);
+console.log(typeof valueInNumb)
+console.log(valueInNumb)
+
+//"33" = 33
+//"33abc" => NaN -> Not a Number
+//true -> 1, false -> 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 -> true
+//"" -> false
+//"divya" -> true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
